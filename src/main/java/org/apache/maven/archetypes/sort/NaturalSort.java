ad5994ded58caf510cb450d86f06ac3cf3d726c6
AlexanderD <dementyev91@gmail.com>
2024-10-24 09:30:56 +0300

    minor update

---
+++ src/main/java/org/apache/maven/archetypes/sort/NaturalSort.java
@@ -0,0 +1,24 @@
+package org.apache.maven.archetypes.sort;
+
+import org.apache.maven.archetypes.classes.AbstractModel;
+
+import java.util.List;
+
+public class NaturalSort {
+    private NaturalSort() {}
+    public static <T extends AbstractModel<T>> void sort(List<T> list) {
+        for (int i = 0; i < list.size(); i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(j).compareTo(list.get(minIndex)) < 0) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                T temp = list.get(i);
+                list.set(i, list.get(minIndex));
+                list.set(minIndex, temp);
+            }
+        }
+    }
+}
