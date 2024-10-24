7145b608b7770fb71bec6559a8fa4237fe2d1dfb
UnderAlex <z-underalex@yander.ru>
2024-10-24 14:31:15 +0500

    Добавлены описания классов и методов Подправлено оформление

--- src/main/java/org/apache/maven/archetypes/writer/AbstractFileWriter.java
+++ src/main/java/org/apache/maven/archetypes/writer/AbstractFileWriter.java
@@ -3,12 +3,12 @@ package org.apache.maven.archetypes.writer;
 import java.io.File;
 
 public abstract class AbstractFileWriter {
+
     String catalog, fileName;
     File file;
 
     public File getFile(){
         if(file == null){
-            System.out.println("Сначала укажите желаемую директорию и имя файла");
             setFile();
         }
         return file;
--- src/main/java/org/apache/maven/archetypes/writer/SortedDataWriter.java
+++ src/main/java/org/apache/maven/archetypes/writer/SortedDataWriter.java
@@ -7,14 +7,25 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
+/**Класс предназначен для записи отсортированных массивов кастомных классов с указанием даты записи, типа объектов
+ * количества объектов и метода сортировки
+ * Для записи в метод необходимо передать следующие параметры:
+ * sortedList - отсортированный массив кастомных классов, наследующих класс AbstractModel
+ * sortName - название алгоритма сортировки
+ * Метод автоматически определяет с каким кастомным классом он работает по переданному list
+ * В зависимости от типа объекта запись происходит в специальный файл, имеющий имя ObjectClassNameSorted.txt
+ * Если файл отсутствует, то он генерируется, если уже есть, то дополняется новыми записями**/
+
 public class SortedDataWriter extends AbstractFileWriter {
 
     void write(List<? extends AbstractModel<?>> sortedList, String sotrName) {
+
         String[] classNameArr = sortedList.getFirst().getClass().toString().split("\\.");
         String className = classNameArr[classNameArr.length-1];
         this.fileName = className + "Sorted.txt";
         //this.catalog = "";//По дефолту длясохранения результатов сортировки будет выбираться папка, гдерасположено приложение
         setFile();
+
         try (FileWriter dataWriter = new FileWriter(getFile(), true)){
             dataWriter.write(String.format("""
                             Дата и время сохранения: %s
@@ -23,13 +34,16 @@ public class SortedDataWriter extends AbstractFileWriter {
                             Метод сортировки: %s
                             """
                     ,DateTimeGetter.getDataTime(), className, sortedList.size(),sotrName));
+
             for(int i = 0; i < sortedList.size(); i++){
                 dataWriter.write(String.format("%d) ", i+1));
                 dataWriter.write(sortedList.get(i).toString());
                 dataWriter.write("\n");
             }
+
             dataWriter.write(DelimeterPrinter.printDelimeter('=',50,2));
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
